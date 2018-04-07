20f8ef0149b23c4fb5c4d833503de484992e7936
Eugene <eformanes@gmail.com>
2018-04-07 04:00:12 -0700

    API Routes work again

--- models/item.js
+++ models/item.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-  title: { type: String, required: true },
-  synopsis: { type: String, required: true },
-  imageURL: { type: String, required: true },
-  episodes: { type: {}, required: true },
+  title: { type: String},
+  description: { type: String},
+  imageURL: { type: String},
+  podCastURL: { type: String},
+  transcription: { type: String},
+  episodes: { type: [String]},
 });
 
 const Item = mongoose.model('Item', itemSchema);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,4 +15,124 @@ module.exports = app => {
           .then(dbModel => res.json(dbModel))
           .catch(err => res.status(422).json(err));
       })
+
+
+      app.get("/api/get/:id", function (req, res){
+        Item.findById(req.params.id, function(req,body){
+            console.log(body);
+            res.send(body);
+        })
+
+    })
+
+    app.post("/api/update/:id", function (req, res){
+        // var query = {'_id':req.params.id};
+        // req.newData.username = req.params.id;
+        // Item.findOneAndUpdate(query,)
+
+
+        var query = { '_id': req.params.id};
+        Item.findOneAndUpdate(query, { transcription: 'Eunji' }, function(){
+            console.log("Updating!");
+        });
+        res.send(true);
+        
+    
+
+    })
+
+
+    ///////********** */
+app.post("/api/transcribe", (req, res) => {
+
+    console.log("Transcribing!");
+    // Imports the Google Cloud client libraries
+  const Storage = require('@google-cloud/storage');
+  const speech = require('@google-cloud/speech');
+  
+  // Creates clients for storage and speech
+  const storage = new Storage();
+  const client = new speech.SpeechClient();
+  
+  
+  // For now manually enter the name of the file name we will test that is already in ./resources/
+  // Currently expecting file is already a mono wav file
+  let audioFileName = 'sos.wav';
+  
+  //Assign const values
+  // Set the google storage bucket
+  const bucketName = 'eearbudsstorage';
+  // Manually set aud
+  let filename = './resources/' + audioFileName;
+  
+  ///--------------------------------//
+  //  Setup for the longRunningRecognize speech function call
+  
+  //build the URI for the file uploaded above
+  const gcsUri = 'gs://' +  bucketName + '/' + audioFileName;
+  // const encoding = 'Eencoding of the audio file, e.g. LINEAR16';
+  // const sampleRateHertz = 16000;
+  const languageCode = 'en-US';
+  
+  const config = {
+      //encoding: encoding,
+      //sampleRateHertz: sampleRateHertz,
+      languageCode: languageCode,
+    };
+    
+    const audio = {
+      uri: gcsUri,
+    };
+    
+    const request = {
+      config: config,
+      audio: audio,
+    };
+  
+  //  End setup for the longRunningRecognize speech function call
+  ///--------------------------------//
+  
+  
+  
+  
+  
+  // Performs the actual upload to the storage bucket specified by "bucketName" const above
+  storage
+    .bucket(bucketName)
+    .upload(filename)
+    .then(() => {
+      console.log(`${filename} uploaded to ${bucketName}.`);
+      // After upload is complete, now call the longRunningRecognize function
+      client
+          .longRunningRecognize(request)
+          .then(data => {
+          const operation = data[0];
+          // Get a Promise representation of the final result of the job
+          return operation.promise();
+          })
+          .then(data => {
+          const response = data[0];
+          const transcription = response.results
+              .map(result => result.alternatives[0].transcript)
+              .join('\n');
+          console.log(`Transcription: ${transcription}`);
+          //console.log(response);
+          })
+          .catch(err => {
+          console.error('ERROR:', err);
+          });
+      ////////
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+  
+    
+  
+    
+  
+    res.send(true);
+  });
+  //////////************ */
+    
 }
